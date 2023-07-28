e39ee35650874a3603a5c516fe225eaa6d655234
5eqn <subcat2077@gmail.com>
2023-07-28 09:39:39 +0800

    implement norm.closure.name

--- src/main/scala/norm/closure/name/Main.scala
+++ src/main/scala/norm/closure/name/Main.scala
@@ -1,7 +1,10 @@
-package norm.hoas.name
+package norm.closure.name
 
 type Name = String
 type Env = Map[Name, Val]
+
+case class Closure(param: Name, env: Env, body: Term):
+  def apply(arg: Val): Val = eval(env + (param -> arg), body)
 
 enum Term:
   case Var(name: Name)
@@ -12,11 +15,11 @@ enum Term:
 enum Val:
   case Var(name: Name)
   case App(func: Val, arg: Val)
-  case Lam(param: Name, value: Val => Val)
+  case Lam(cl: Closure)
 
   def apply(u: Val): Val = this match {
-    case Lam(_, t) => t(u)
-    case t         => App(t, u)
+    case Lam(cl) => cl.apply(u)
+    case t       => App(t, u)
   }
 
 def fresh(ns: List[Name], x: Name): Name = x match {
@@ -29,7 +32,7 @@ def eval(env: Env, tm: Term): Val = tm match {
   case Term.Var(name) =>
     env.get(name).get
   case Term.Lam(param, body) =>
-    Val.Lam(param, arg => eval(env + (param -> arg), body))
+    Val.Lam(Closure(param, env, body))
   case Term.App(func, arg) =>
     eval(env, func).apply(eval(env, arg))
   case Term.Let(name, body, next) =>
@@ -41,9 +44,9 @@ def quote(ns: List[Name], x: Val): Term = x match {
     Term.Var(name)
   case Val.App(func, arg) =>
     Term.App(quote(ns, func), quote(ns, arg))
-  case Val.Lam(param, value) =>
-    val name = fresh(ns, param)
-    Term.Lam(name, quote(name :: ns, value(Val.Var(name))))
+  case Val.Lam(cl) =>
+    val name = fresh(ns, cl.param)
+    Term.Lam(name, quote(name :: ns, cl.apply(Val.Var(name))))
 }
 
 def nf(env: Env, tm: Term): Term = quote(env.keys.toList, eval(env, tm))
--- src/test/scala/TestSuite.scala
+++ src/test/scala/TestSuite.scala
@@ -1,9 +1,17 @@
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
 class TestSuite extends munit.FunSuite {
-  test("normalize by evaluation") {
-    import com.elab.norm.hoas.name._
-    val sampleEnv: Env = List(("y", Val.Var("y")))
+  test("norm.hoas.name") {
+    import norm.hoas.name._
+    val sampleEnv: Env = Map("y" -> Val.Var("y"))
+    val sampleTerm: Term = Term.App(Term.Lam("x", Term.Var("x")), Term.Var("y"))
+    val expectedNormalizedForm: Term = Term.Var("y")
+    val result = nf(sampleEnv, sampleTerm)
+    assertEquals(result, expectedNormalizedForm)
+  }
+  test("norm.closure.name") {
+    import norm.closure.name._
+    val sampleEnv: Env = Map("y" -> Val.Var("y"))
     val sampleTerm: Term = Term.App(Term.Lam("x", Term.Var("x")), Term.Var("y"))
     val expectedNormalizedForm: Term = Term.Var("y")
     val result = nf(sampleEnv, sampleTerm)
