7e05d9cfb65785cc6afb3fdcf3225ad851e9600c
5eqn <491100866@qq.com>
2023-07-28 09:11:33 +0800

    implement normalization (hoas, name)

---
+++ src/test/scala/TestSuite.scala
@@ -0,0 +1,12 @@
+// For more information on writing tests, see
+// https://scalameta.org/munit/docs/getting-started.html
+class TestSuite extends munit.FunSuite {
+  test("normalize by evaluation") {
+    val sampleEnv: Env = List(("y", Val.Var("y")))
+    val sampleTerm: Term = Term.App(Term.Lam("x", Term.Var("x")), Term.Var("y"))
+    val expectedNormalizedForm: Term = Term.Var("y")
+    val result = nf(sampleEnv, sampleTerm)
+
+    assertEquals(result, expectedNormalizedForm)
+  }
+}
