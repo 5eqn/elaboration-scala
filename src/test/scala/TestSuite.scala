e23baa174d1cb50cf0fe0841a1239efc6e306087
5eqn <subcat2077@gmail.com>
2023-07-28 16:11:42 +0800

    implement norm.closure.debruijn

--- src/test/scala/TestSuite.scala
+++ src/test/scala/TestSuite.scala
@@ -3,25 +3,41 @@
 class TestSuite extends munit.FunSuite {
   test("norm.hoas.name") {
     import norm.hoas.name._
-    val sampleEnv: Env = Map("y" -> Val.Var("y"))
-    val sampleTerm: Term = Term.App(Term.Lam("x", Term.Var("x")), Term.Var("y"))
-    val expectedNormalizedForm: Term = Term.Var("y")
+    val sampleEnv: Env = Map("a" -> Val.Var("a"), "g" -> Val.Var("g"))
+    val sampleTerm: Term = Term.Let(
+      "f",
+      Term.Lam("x", Term.Var("x")),
+      Term.App(Term.Var("f"), Term.App(Term.Var("g"), Term.Var("f")))
+    )
+    val expectedNormalizedForm: Term =
+      Term.App(Term.Var("g"), Term.Lam("x", Term.Var("x")))
     val result = nf(sampleEnv, sampleTerm)
     assertEquals(result, expectedNormalizedForm)
   }
+
   test("norm.closure.name") {
     import norm.closure.name._
-    val sampleEnv: Env = Map("y" -> Val.Var("y"))
-    val sampleTerm: Term = Term.App(Term.Lam("x", Term.Var("x")), Term.Var("y"))
-    val expectedNormalizedForm: Term = Term.Var("y")
+    val sampleEnv: Env = Map("a" -> Val.Var("a"), "g" -> Val.Var("g"))
+    val sampleTerm: Term = Term.Let(
+      "f",
+      Term.Lam("x", Term.Var("x")),
+      Term.App(Term.Var("f"), Term.App(Term.Var("g"), Term.Var("f")))
+    )
+    val expectedNormalizedForm: Term =
+      Term.App(Term.Var("g"), Term.Lam("x", Term.Var("x")))
     val result = nf(sampleEnv, sampleTerm)
     assertEquals(result, expectedNormalizedForm)
   }
+
   test("norm.closure.debruijn") {
     import norm.closure.debruijn._
-    val sampleEnv: Env = List(Val.Var(0))
-    val sampleTerm: Term = Term.App(Term.Lam(Term.Var(0)), Term.Var(1))
-    val expectedNormalizedForm: Term = Term.Var(1)
+    val sampleEnv: Env = List(Val.Var(1), Val.Var(0))
+    val sampleTerm: Term = Term.Let(
+      Term.Lam(Term.Var(0)),
+      Term.App(Term.Var(0), Term.App(Term.Var(2), Term.Var(0)))
+    )
+    val expectedNormalizedForm: Term =
+      Term.App(Term.Var(1), Term.Lam(Term.Var(0)))
     val result = nf(sampleEnv, sampleTerm)
     assertEquals(result, expectedNormalizedForm)
   }
