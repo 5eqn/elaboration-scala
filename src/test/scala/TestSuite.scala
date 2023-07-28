37704c2b09f2f9f823a7ac2836c5aa404a7bdaf0
5eqn <subcat2077@gmail.com>
2023-07-28 09:17:21 +0800

    enhance project structure

--- src/test/scala/TestSuite.scala
+++ src/test/scala/TestSuite.scala
@@ -2,11 +2,11 @@
 // https://scalameta.org/munit/docs/getting-started.html
 class TestSuite extends munit.FunSuite {
   test("normalize by evaluation") {
+    import com.elab.norm.hoas.name._
     val sampleEnv: Env = List(("y", Val.Var("y")))
     val sampleTerm: Term = Term.App(Term.Lam("x", Term.Var("x")), Term.Var("y"))
     val expectedNormalizedForm: Term = Term.Var("y")
     val result = nf(sampleEnv, sampleTerm)
-
     assertEquals(result, expectedNormalizedForm)
   }
 }
